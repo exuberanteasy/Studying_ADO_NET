3c041a1f1dbe062b006b879106be5bb864648230
exuberanteasy <exuberanteasy@gmail.com>
2018-09-05 18:22:35 +0800

    Update and rename 引用ADO.NET命名空間.note to 引用ADO.NET命名空間.cs

---
+++ 引用ADO.NET命名空間.cs
@@ -0,0 +1,16 @@
+引用ADO.NET命名空間
+System.Data
+
+Tables
+Rows
+Columns
+Constraints
+DataSet
+
+寫法如下:
+using System.Data;  //引用 ADO.NET 基礎物件
+.
+System.Data.OleDb  //引用OLE DB 資料來源物件
+.
+System.Data.SqlClient
+using System.Data.SqlClient; //引用 SQL Server 資料來源物件
