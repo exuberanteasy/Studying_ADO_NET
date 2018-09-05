032a8f5d3553728f3017198972429a8c64bb68d8
exuberanteasy <exuberanteasy@gmail.com>
2018-09-05 10:28:05 +0800

    Update 如何使用using與Connection物件連接資料庫.cs

--- 如何使用using與Connection物件連接資料庫.cs
+++ 如何使用using與Connection物件連接資料庫.cs
@@ -2,7 +2,7 @@
 
 using System.Data.SqlClient;
 
-namespace TestConnection
+namespace ConnectionDemo_2
 {
     public partial class Form1 : Form
     {
@@ -13,12 +13,14 @@ namespace TestConnection
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            using (SqlConnection cn = new SqlConnection() )
+            using (SqlConnection cn = new SqlConnection() ) //建立 cn 為 SqlConnection物件
             {
                 cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
                                       "AttachDbFilename=|DataDirectory|Northwind.mdf;" +
                                       "Integrated Security=True";
+                // 處理資料庫的敘述
                 cn.Open();
+                // 當離開 using 區塊時，馬上執行 Dispose()方法 釋放 Connection物件的 所佔用的資源
                 if (cn.State == ConnectionState.Open)
                 {
                     MessageBox.Show("資料庫已連接", "目前狀態");
