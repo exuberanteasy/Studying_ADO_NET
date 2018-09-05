0bc7c8924d1c8f0901db39b804c450873950ae54
exuberanteasy <exuberanteasy@gmail.com>
2018-09-05 21:26:07 +0800

    Update 使用SQL語法的注意事項.cs

--- 使用SQL語法的注意事項.cs
+++ 使用SQL語法的注意事項.cs
@@ -11,22 +11,21 @@ namespace SqlStringDemo1
             InitializeComponent();
         }
 
-        private void btnSearch_Click(object sender, EventArgs e)
+        private void txtSearch_Click(object sender, EventArgs e)
         {
-            //使用 using 敘述建立 SqlConnection 物件 cn
             using (SqlConnection cn = new SqlConnection())
             {
-                // 建立字串指定 ch16DB.mdf 資料庫
-                cn.ConnectionString= @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
                                       "AttachDbFilename=|DataDirectory|ch16DB.mdf;" +
                                       "Integrated Security=True";
+
                 cn.Open();
-                //將輸入的姓名指定給 searchName 字串變數
+
                 string searchName = txtName.Text;
 
 
                 //SELECT 敘述的查詢條件為姓名等於 searchName
-                string selectCmd = "SELECT * FROM 成績單 WHERE 姓名 = '" + 
+                string selectCmd = "SELECT * FROM 成績單 WHERE 姓名 = '" +
                        searchName + "'";
 
                 //建立 SqlCommand 物件 cmd
@@ -41,9 +40,9 @@ namespace SqlStringDemo1
 
                     txtShow.Text += "姓名: " + dr["姓名"].ToString() + Environment.NewLine;
 
-                    txtShow.Text += "中文: " + dr["中文"].ToString() + Environment.NewLine;
+                    txtShow.Text += "國文: " + dr["國文"].ToString() + Environment.NewLine;
 
-                    txtShow.Text += "English" + dr["English"].ToString() + Environment.NewLine;
+                    txtShow.Text += "英文" + dr["英文"].ToString() + Environment.NewLine;
 
                     txtShow.Text += "數學" + dr["數學"].ToString();
                 }
@@ -51,6 +50,9 @@ namespace SqlStringDemo1
                 {
                     txtShow.Text = "找不到這個學生的成績!";
                 }
+            }
+            {
+
             }
         }
     }
