6243cfff467523d641ff5d12aeb08e1690f6eb20
exuberanteasy <exuberanteasy@gmail.com>
2018-09-11 17:35:49 +0800

    Update DataReader_常用屬性.cs

--- DataReader_常用屬性.cs
+++ DataReader_常用屬性.cs
@@ -1,5 +1,32 @@
 DataReader 常用屬性
 
+//===== Depth_深度 ==========================================================================
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
+namespace test_DataReader
+{
+    public partial class DataReader_Depth : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection Conn = new SqlConnection(WebConfigurationManager
+                .ConnectionStrings["testConnectionString"].ConnectionString);
+            Conn.Open();  //---- 連接 DB -----
 
+            SqlCommand cmd = new SqlCommand("select id, test_time, summary, author from test", Conn);
 
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+            Response.Write("SqldataReader的 .Depth屬性--" + dr.Depth);
+            //--- SqlDataReader的 .Depth屬性--目前資料列的巢狀深度。----------
+            
+            cmd.Cancel();
+            dr.Close();
+            Conn.Close();
+        }
+    }
+}
+
+//===============================================================================
