393eb7946b46cdd9d9c003af6b4310f9304157e0
exuberanteasy <exuberanteasy@gmail.com>
2018-09-11 17:49:37 +0800

    Update DataReader_常用屬性.cs

--- DataReader_常用屬性.cs
+++ DataReader_常用屬性.cs
@@ -35,7 +35,33 @@ namespace test_DataReader
 }
 
 //===== FieldCount 屬性 ==========================================================================
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
+namespace test_DataReader
+{
+    public partial class DataReader_Depth : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection Conn = new SqlConnection(WebConfigurationManager
+                .ConnectionStrings["存在Web.Config檔案裡面的 DB 連接字串"].ConnectionString);
+            Conn.Open();  //---- 連接 DB -----
+
+            SqlCommand cmd = new SqlCommand("select id, test_time, summary, author from test", Conn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+            Label1.Text = "test資料表，共有幾個欄位?......" + dr.FieldCount.ToString();
+            //------ SqlDataReader的 .FieldCount 屬性--目前一筆紀錄的資料行(欄位、Column)數。 ----
+            
+            cmd.Cancel();
+            dr.Close();
+            Conn.Close();
+        }
+    }
+}
 
 
 
