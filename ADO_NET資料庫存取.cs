78c30b789be3e536b619fb7db9bc7495ca96aec4
exuberanteasy <exuberanteasy@gmail.com>
2018-09-14 18:17:31 +0800

    Update ADO_NET資料庫存取.cs

--- ADO_NET資料庫存取.cs
+++ ADO_NET資料庫存取.cs
@@ -53,3 +53,33 @@ namespace ConnectionDemo1
         }
     }
 }
+//----------------------------------------------
+//---FileName: Form1.cs-----------
+//  16-11
+using System.Data.SqlClient;
+
+namespace ConnectionDemo_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                    "AttachDbFilename = |DataDirectory|Northwind.mdf;" +
+                    "Integrated Security=True";
+                cn.Open();
+                if (cn.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("資料庫已連接", "目前狀態");
+                }
+            }
+        }
+    }
+}
