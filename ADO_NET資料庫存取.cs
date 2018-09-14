a077879503dc993361ab41862f3d37bd13f9c840
exuberanteasy <exuberanteasy@gmail.com>
2018-09-14 18:05:10 +0800

    Create ADO_NET資料庫存取.cs

---
+++ ADO_NET資料庫存取.cs
@@ -0,0 +1,54 @@
+如何Connection物件
+
+using System.Data.SqlClient;
+
+namespace ConnectionDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection();
+
+        //定義 ShowConnection 用來txtShow上顯示資料來源的相關資訊
+        private void ShowConnection()
+        {
+            textBox1.Text = "連接字串: " + cn.ConnectionString + Environment.NewLine;
+            textBox1.Text += "逾時秒數: " + cn.ConnectionTimeout + Environment.NewLine;
+            textBox1.Text += "資料庫: " + cn.Database + Environment.NewLine;
+            textBox1.Text += "資料來源:" + cn.DataSource + Environment.NewLine;
+        }
+        //表單載入時執行此事件
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //設定連接字串，用來連接Northwind.mdf 資料庫
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilneFilename=|DataDirectory|Northwind.mdf;" +
+                "Integrated Security=True";
+            ShowConnection();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //判斷目前是否為資料庫關閉狀態
+            if (cn.State == ConnectionState.Closed)
+            {
+                cn.Open();
+                button1.Text = "關閉";
+                textBox1.Text += "目前狀態:資料庫已連接" + Environment.NewLine;
+
+            }
+
+            else if (cn.State == ConnectionState.Open)
+            {
+                cn.Close();
+                button1.Text = "開啟";
+                textBox1.Text += "目前狀態:資料庫已關閉" + Environment.NewLine;
+            }
+            ShowConnection();
+        }
+    }
+}
